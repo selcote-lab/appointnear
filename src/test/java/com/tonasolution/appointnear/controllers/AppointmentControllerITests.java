b73216b28ef407dfb1b9565931c230ebad418767
EL AKIOUI  Zouhaire <zouhaire.elakioui@gmail.com>
2019-11-07 16:38:16 +0100

    remove space added

--- src/test/java/com/tonasolution/appointnear/controllers/AppointmentControllerITests.java
+++ src/test/java/com/tonasolution/appointnear/controllers/AppointmentControllerITests.java
@@ -27,7 +27,6 @@ import com.tonasolution.appointnear.models.IAppointment;
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application.properties")
 public class AppointmentControllerITests {
-	
 	public static final String API_URL = "/api/v1/appointments/";
 	
 	@Autowired
